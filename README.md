347f9113e975cbff43fca3031ab784c498a795e1
Aleksei Chikin <mail@makingoff.name>
2017-11-25 00:34:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# npm-runner package
+
+Experimental npm task runner.
+
+![Package preview](http://scrns.s3.amazonaws.com/Npm_runner__Projectsaquaflat_2017-11-25_00-31-51.png)
---
+++ spec/npm-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NpmRunnerView from '../lib/npm-runner-view';
+
+describe('NpmRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
